9a6989f14719bdc86e8fd61d4dd5e8833a96cf39
XDCorvus-Xtacy <doxa0430@gmail.com>
2025-10-30 12:14:59 +0000

    start making gomoku

---
+++ gomoku.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+
+#define SIZE 19
+
+//보드 출력 함ㅅ
+void printBoard(char board[SIZE][SIZE])
+{
+    printf("\n");
+
+}
+
+
+//메인 함수
+int main(void)
+{
+    char board[SIZE][SIZE] = 0
+    for (i=0; i<SIZE; i++)
+        for (j=0; j<SIZE; j++)
+            board[i][j] = ' ';
+
+    
+}
